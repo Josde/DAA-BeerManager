05eef743e090cadbbff9a91734de82386a564de7
Jorge Cruz <jorgecruz@usal.es>
2021-01-01 08:14:29 -0800

    added tableview parsing

---
+++ BeerManager/RowCell.swift
@@ -0,0 +1,15 @@
+//
+//  RowCell.swift
+//  BeerManager
+//
+//  Created by Jorge Cruz on 01/01/2021.
+//  Copyright © 2021 Jorge Cruz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RowCell : UITableViewCell {
+    @IBOutlet weak var beerPhoto:UIImageView?
+    @IBOutlet weak var beerName:UILabel?
+}
