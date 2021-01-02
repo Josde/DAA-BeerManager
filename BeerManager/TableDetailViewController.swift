9b145e5a3aead356ab86f57d4f8647650a99ce32
Jorge Cruz <jorgecruz@usal.es>
2021-01-01 16:56:25 -0800

    Added segue + basic deletion functionality.

---
+++ BeerManager/TableDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableDetailViewController.swift
+//  BeerManager
+//
+//  Created by Jorge Cruz on 01/01/2021.
+//  Copyright © 2021 Jorge Cruz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TableDetailViewController : UIViewController {
+    var beer : Beer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
