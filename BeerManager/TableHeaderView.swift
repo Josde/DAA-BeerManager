204be47f0605c3627e0cb0c2160232676007e48c
Jorge Cruz <jorgecruz@usal.es>
2021-01-15 12:22:30 -0800

    Final Commit! Serialization done 100% BeerDetail ScrollView done Sorting done Fully refactored model Refactored everything in general Added launch screen idk what more to say lol

---
+++ BeerManager/TableHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  TableHeader.swift
+//  BeerManager
+//
+//  Created by Jorge Cruz on 14/01/2021.
+//  Copyright © 2021 Jorge Cruz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TableHeaderView : UITableViewHeaderFooterView {
+    //MARK: Outlets
+    @IBOutlet weak var name: UILabel!
+}
